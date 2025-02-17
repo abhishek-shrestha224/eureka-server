a669be2ffbc6273857d5cc83ca656e1a7d178e62
Abhishek Shrestha <meabhi224@gmail.com>
2025-02-17 23:38:57 +0545

    init

---
+++ src/test/java/world/hello/service_discovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package world.hello.service_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
